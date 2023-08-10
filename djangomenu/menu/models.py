95e990348a20afce296329e7ced3c2b9cc30b379
Maryia <m.snarava@mageworx.com>
2023-08-10 20:16:45 +0400

    Add classes for cafe objects

--- djangomenu/menu/models.py
+++ djangomenu/menu/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# An inventory of different Ingredients, their available quantity, and their prices per unit
+class Ingredient(models.Model):
+    pass
+
+#A list of the restaurant’s MenuItems, and the price set for each entry
+class MenuItem(models.Model):
+    pass
+
+#A list of the ingredients that each menu item requires (RecipeRequirements)
+class RecipeRequirement(models.Model):
+    pass
+
+#A log of all Purchases made at the restaurant
+class Purchase(models.Model):
+    pass

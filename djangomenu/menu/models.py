9364a12691b148f40746bb861460230f6a2e975c
Maryia <m.snarava@mageworx.com>
2023-08-10 20:53:06 +0400

    Add fields to models

--- djangomenu/menu/models.py
+++ djangomenu/menu/models.py
@@ -2,16 +2,43 @@ from django.db import models
 
 # An inventory of different Ingredients, their available quantity, and their prices per unit
 class Ingredient(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    quantity = models.FloatField(default=0.0)
+    unit = models.CharField(max_length=30)
+    unit_price = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+      return "list"
 
 #A list of the restaurant’s MenuItems, and the price set for each entry
 class MenuItem(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    price = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return "list"
 
 #A list of the ingredients that each menu item requires (RecipeRequirements)
 class RecipeRequirement(models.Model):
-    pass
+    menuItem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0.0)
+
+    def get_absolute_url(self):
+        return "list"
 
 #A log of all Purchases made at the restaurant
 class Purchase(models.Model):
-    pass
+    menuItem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    def __str__(self):
+        return self.menuItem.name
+
+    def get_absolute_url(self):
+        return "list"
